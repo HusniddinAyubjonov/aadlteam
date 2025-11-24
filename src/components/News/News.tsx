85bc3286dc95ea810927f733b674cadf262b7f8d
HusniddinAyubjonov <husniddinayubjonov@gmail.com>
2025-11-24 21:45:22 +0300

    added section News

--- src/components/News/News.tsx
+++ src/components/News/News.tsx
@@ -1,3 +1,4 @@
+import { New } from "./Constants";
 import style from "./News.module.css";
 
 export default function News() {
@@ -11,6 +12,26 @@ export default function News() {
             neque orci amet, amet .
           </p>
         </span>
+
+        <div className={style.cards}>
+          {New.map((info, index) => (
+            <div key={index} className={style.card}>
+              <img src={info.img} alt='image' />
+              <button>{info.btnText}</button>
+              <h3>{info.title}</h3>
+              <p>{info.description}</p>
+              <hr />
+              <div className={style.user}>
+                <img src={info.avatar} alt='avatar' />
+                <span>
+                  <h4>{info.full_name}</h4>
+                  <h5>{info.about_user}</h5>
+                </span>
+              </div>
+            </div>
+          ))}
+        </div>
+        <button className={style.moreBtn}>View All Articles</button>
       </div>
     </section>
   );
